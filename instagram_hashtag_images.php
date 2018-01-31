795eb1f65384f2d1d969dfab76a5e7408e969379
Ahsan Khan <bestkhan92@gmail.com>
2018-01-31 21:11:26 +0500

    little } bug

--- instagram_hashtag_images.php
+++ instagram_hashtag_images.php
@@ -19,7 +19,7 @@ $image_array= array(); // array to store images.
 	 	$image_data  = '<img src="'.$latest_array['thumbnail_src'].'">'; // thumbnail and same sizes 
 	 	//$image_data  = '<img src="'.$latest_array['display_src'].'">'; actual image and different sizes 
 		array_push($image_array, $image_data);
-	
+	}
 	foreach ($image_array as $image) {
 		echo $image;// this will echo the images wrap it in div or ul li what ever html structure 
 	}
