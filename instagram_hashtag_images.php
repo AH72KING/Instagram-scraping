f9dfbcbab08814080bf34af7083962c0560d3c28
Ahsan Khan <bestkhan92@gmail.com>
2018-01-31 21:10:12 +0500

    update after vold4@seznam.cz mail
    Code is updated after instagram has changed there json data format

--- instagram_hashtag_images.php
+++ instagram_hashtag_images.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 function scrape_insta_hash($tag) {
 	$insta_source = file_get_contents('https://www.instagram.com/explore/tags/'.$tag.'/'); // instagrame tag url
 	$shards = explode('window._sharedData = ', $insta_source);
@@ -8,14 +8,18 @@ function scrape_insta_hash($tag) {
 }
 $tag = 'pakistan'; // tag for which ou want images 
 $results_array = scrape_insta_hash($tag);
-$limit = 7; // provide the limit thats important because one page only give some images then load more have to be clicked
+//$limit = 7; //previous was only limit to 7 images
+$limit = 56; // provide the limit thats important because one page only give some images.
 $image_array= array(); // array to store images.
 	for ($i=0; $i < $limit; $i++) { 
-		$latest_array = $results_array['entry_data']['TagPage'][0]['tag']['media']['nodes'][$i];
+		//previous code to get images from json 	
+		//$latest_array = $results_array['entry_data']['TagPage'][0]['tag']['media']['nodes'][$i];	
+		//new code to get images from json 	
+		$latest_array = $results_array['entry_data']['TagPage'][0]['graphql']['hashtag']['edge_hashtag_to_media']['edges'][$i]['node'];
 	 	$image_data  = '<img src="'.$latest_array['thumbnail_src'].'">'; // thumbnail and same sizes 
 	 	//$image_data  = '<img src="'.$latest_array['display_src'].'">'; actual image and different sizes 
 		array_push($image_array, $image_data);
-	}
+	
 	foreach ($image_array as $image) {
 		echo $image;// this will echo the images wrap it in div or ul li what ever html structure 
 	}
